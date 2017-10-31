5ccc58bdad7a4aa19de3ad898d779bcfbe9d1972
JJSomiya <jjsomiya.gmail.com>
2017-10-31 09:28:13 +0800

    Fibonacci sequence

--- easy/ClimbingStairs.py
+++ easy/ClimbingStairs.py
@@ -16,7 +16,31 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        if n <= 2:
+            return 1
 
+        a, b = 1, 2
+        for _ in range(2, n):
+            a, b = b, a + b
+
+        return b
+
+        # if n <= 0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+        # if n == 2:
+        #     return 2
+        # a = 1
+        # b = 2
+        # all_ways = 0
+        # while (n > 2):
+        #     n -= 1
+        #     all_ways = a + b
+        #     a = b
+        #     b = all_ways
+        #
+        # return all_ways
 
 if __name__ == '__main__':
     s = Solution()
