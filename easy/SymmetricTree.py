6864234400c2aba350fc3f2239d4e8e24973def5
Somiya <jjsomiya@gamil.com>
2017-11-02 09:36:03 +0800

    Symmetric Tree

--- easy/SymmetricTree.py
+++ easy/SymmetricTree.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
 
+if __name__ == "__main__":
+    Solution().isSymmetric();
